4a7ec040274fbbc9ad7e3d4dbfafde5bad9f5bc7
Alexandra Chernova <estelelendil@mail.ru>
2023-06-06 17:40:06 +0300

    added lazy `jsonFetch`

--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,11 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState, useCallback } from "react"
 
-export default function useJsonFetch(url, opts) {
+export default function useJsonFetch(url, searchParams) {
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     useEffect(() => {
-        url != null && fetch(url).then((response) => {
+        url != null && fetch({ url, searchParams }).then((response) => {
             setLoading(false);
             console.log('response', response);
 
@@ -15,8 +15,30 @@ export default function useJsonFetch(url, opts) {
             return response.json()
         }).then((answer) => setData((answer))).catch((err) => setError(err))
 
-    }, [url])
+    }, [searchParams, url])
     return [data, loading, error]
+}
 
+export function useLazyJsonFetch(url, searchParams) {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    const callback = useCallback(
+        (url, searchParams) => {
+            url != null && fetch({ url, searchParams }).then((response) => {
+                setLoading(false);
+                console.log('response', response);
+
+                if (response.status !== 200) {
+                    throw new Error(response.status)
+                }
+                return response.json()
+            }).then((answer) => setData((answer))).catch((err) => setError(err))
+        },
+        [],
+    );
+
+    return [data, loading, error, callback]
 }
 
