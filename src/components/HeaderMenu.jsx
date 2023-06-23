77db4483473bbe776749c2c19489596c77497fbe
Alexandra Chernova <estelelendil@mail.ru>
2023-06-23 17:36:29 +0300

    correct set Step

--- src/components/HeaderMenu.jsx
+++ src/components/HeaderMenu.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import Logo from "./Logo";
 
-export default function HeaderMenu() {
+export default function HeaderMenu({ setStep }) {
   return (
     <div className="w-full flex flex-col">
       <Logo />
-      <div className="w-full h-[94px] items-center bg-[#292929] flex gap-[84px] pl-[258px] py-[10px]">
-        <NavLink className=" text-white text-30" to="/">
+      <div
+        onClick={() => setStep(0)}
+        className="w-full h-[94px] items-center bg-[#292929] flex gap-[84px] pl-[258px] py-[10px]"
+      >
+        <NavLink className=" text-white text-30" to="/about-us">
           O Нас
         </NavLink>
         <NavLink className=" text-white text-30" to="/how-it-works">
