72d7674b5aa90c8db9b006692b3b89d2acc6c592
Alexandra Chernova <estelelendil@mail.ru>
2023-08-31 18:49:26 +0300

    added getRandomInt helper and config

--- src/helpers.js
+++ src/helpers.js
@@ -9,3 +9,10 @@ export const PriceSum = (arr, num) => {
     });
     return sum;
 };
+
+export function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
