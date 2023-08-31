b383cad8c315aba035514550f423099c5f214010
Alexandra Chernova <estelelendil@mail.ru>
2023-08-31 10:59:40 +0300

    paymentCard and redirect to `PaymentCard`

--- src/components/SearchResults/Coach/Persons/ConnectedPersons.jsx
+++ src/components/SearchResults/Coach/Persons/ConnectedPersons.jsx
@@ -1,50 +1,145 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useQuery } from "../../../../utils";
 import TripDetails from "./TripDetails";
 import PassengersCard from "./PassengersCard";
 import MyButton from "../../../UI/MyButton";
+import { isEmpty } from "lodash";
+import PaymentCard from "./PaymentCard";
+import { useNavigate } from "react-router";
 
 export default function ConnectedPersons() {
   let query = useQuery();
   const coach = query.get("coach");
+  const direction = query.get("direction");
+  const [info, setInfo] = useState({});
+  const [nextPayment, setNextPayment] = useState(false);
+  // const [user, setUser] = useState({});
+  const navigate = useNavigate();
+  useEffect(() => {
+    let rawValue = localStorage.getItem("tripInfo");
+    while (rawValue === null) {
+      console.log("tripInfo is null");
+      rawValue = localStorage.getItem("tripInfo");
+    }
+    const newTripInfo = rawValue !== null ? rawValue : "{}";
+    setInfo(JSON.parse(newTripInfo));
+  }, []);
+
   const [persons, setPersons] = useState([{ id: 1 }]);
+
   const addPerson = (obj) => {
-    console.log("OBJECT FORM PERSONS", obj);
-    // console.log("OBJECT FORM PERSONS", obj);
+    console.log("ADD PERSONS", obj);
     setPersons((prev) => {
       const index = prev.findIndex((item) => item.id === obj.id);
+      console.log("Индекc", index, prev, obj);
       if (index === -1) {
         return [...prev, obj];
       } else {
-        return prev.splice(index, 1, obj);
+        const newPrev = [...prev];
+        const deleted = newPrev.splice(index, 1, obj);
+        console.log("Добавление данных формы", deleted, newPrev);
+        return newPrev;
       }
     });
   };
-
   const addPassenger = () => {
-    addPerson({ id: persons.length + 1 });
-    // return <PassengersCard index={persons.length + 1} setPerson={setPerson} persons={persons} />;
+    console.log("addPassenger");
+    addPerson({ id: persons[persons.length - 1].id + 1 });
   };
-  return (
-    <div className="flex">
-      PERSONS
-      <TripDetails id={coach} />
-      {/* <div> */}
-      {/* прокидывать номер как id */}
-      <div className="flex flex-col gap-[30px] mt-[36px] pl-[75px]">
-        {persons.map((item) => {
-          return <PassengersCard key={item.id} index={item.id} setPerson={addPerson} person={persons[item.id - 1]} />;
-        })}
 
-        {/* </div> */}
-        <div className="w-full">
-          <MyButton
-            className={" w-full"}
-            label="Добавить пассажира"
-            color="transparent"
-            onClick={() => addPassenger()}
-          ></MyButton>
-        </div>
+  const removePassenger = (id) => {
+    if (persons.length === 1) {
+      return;
+    }
+    const index = persons.findIndex((item) => item.id === id);
+    if (index === -1) {
+      console.log("-1", id);
+      return;
+    } else {
+      setPersons((prev) => {
+        const newPersons = [...prev];
+        const deleted = newPersons.splice(index, 1);
+        console.log("index", index, deleted, newPersons);
+        return newPersons;
+      });
+    }
+  };
+  useEffect(() => {
+    console.log("UE persons", persons);
+  }, [persons]);
+  return (
+    <div className="flex justify-center">
+      {!isEmpty(info) && <TripDetails item={info} persons={persons} />}
+
+      <div className="flex flex-col gap-[30px] mt-[36px] pl-[75px]">
+        {nextPayment ? (
+          <PaymentCard
+            info={info}
+            persons={persons}
+            directionId={direction}
+            coachId={coach}
+            goBack={() => setNextPayment(false)}
+          />
+        ) : (
+          persons.map((item, index) => {
+            return (
+              <PassengersCard
+                key={item.id}
+                index={index}
+                setPerson={addPerson}
+                person={item}
+                removePers={removePassenger}
+              />
+            );
+          })
+        )}
+        {/* {persons.length !== 0 &&
+            persons.every((item) => {
+              if (!item.name) {
+                toast.error("Заполните данные пассажиров");
+                return false;
+              }
+            }) && ( */}
+        {!nextPayment && (
+          <div className="w-full mb-6">
+            {info.price && persons.length < info.price.length && (
+              <MyButton
+                className={" w-full"}
+                label="Добавить пассажира"
+                color="transparent"
+                onClick={() => addPassenger()}
+              ></MyButton>
+            )}
+            <div className="flex justify-between mt-6">
+              <MyButton
+                className={" w-[250px] p-4 h-[100px]"}
+                label="Назад"
+                color="transparent"
+                onClick={() => navigate(-1)}
+              ></MyButton>
+              <MyButton
+                label="Далее"
+                className={"w-[250px] self-end"}
+                color="orange"
+                onClick={() => {
+                  console.log("Persons", persons);
+                  // if (
+                  //   persons.every((item) => {
+                  //     if (!item.name) {
+                  //       return false;
+                  //     }
+                  //   })
+                  // ) {
+                  //   console.log("Заполните даннние пассажиров");
+                  // } else {
+                  setNextPayment(true);
+                  // }
+                }}
+              ></MyButton>
+            </div>
+          </div>
+        )}
+        {/* )} */}
       </div>
     </div>
   );
---
+++ src/components/SearchResults/Coach/Persons/PaymentCard.jsx
@@ -0,0 +1,155 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import MyInput from "../../../UI/MyInput";
+import classNames from "classnames";
+import MyButton from "../../../UI/MyButton";
+import FinalPage from "../../FinalPage";
+import { isEmpty } from "lodash";
+
+export default function PaymentCard({ goBack, persons, coachId, info, directionId }) {
+  const { control, handleSubmit, setValue } = useForm(true);
+  const [finalData, setFinalData] = useState({});
+  const [payCash, setPayCash] = useState(isEmpty(finalData) ? true : finalData.user.payment_method);
+  const [nextPage, setNextPage] = useState(false);
+  const onSubmit = (data) => {
+    console.log("submitData", data);
+    if (data) {
+      const seats = [];
+      persons.map((item, index) => {
+        seats.push({
+          coach_is: coachId,
+          person_info: item,
+          seat_number: info.chosenSeats[index],
+          is_child: !item.is_adult,
+          include_children_seat: item.include_children_seat,
+        });
+        console.log("seats", seats);
+        return null;
+      });
+      const finalObj = {
+        user: { ...data, payment_method: payCash ? "cash" : "online" },
+        departure: {
+          route_direction_id: directionId,
+          seats,
+        },
+      };
+      setFinalData(finalObj);
+      setNextPage(true);
+    }
+    // setUser({ ...data, payment_method: payCash ? "cash" : "online" });
+  };
+  const choosenCash = classNames({
+    "p-2 border-1  rounded-[5px]  w-[25px] h-[25px] ": true,
+    " border-slate-200 text-slate-700 ": !payCash,
+    "flex items-center text-[#FFA800] text-14 border-[#FFA800] ": payCash,
+  });
+  const choosenOnline = classNames({
+    "p-2 border-1 rounded-[5px] w-[25px] h-[25px]": true,
+    " border-slate-200 text-slate-700 ": payCash,
+    "flex items-center text-orange border-[#FFA800] text-center ": !payCash,
+  });
+  return (
+    <>
+      {nextPage ? (
+        <FinalPage data={finalData} goBack={() => setNextPage(false)} />
+      ) : (
+        <div>
+          <div className="flex flex-col gap-[25px] border-1 border-slate-400 w-[830px] pb-6 ">
+            <div className="bg-[#F9F9F9] flex justify-between items-center pr-8 h-[107px] w-full border-b-1 border-[#d9d9d9]">
+              <h3 className="text-30 py-8 pl-8 ">{"Персональные данные"}</h3>
+            </div>
+            {/* <button className="text-gray">❌</button> */}
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              className="flex flex-col gap-[25px] [&_span]:text-gray [&_span]:text-16 [&_span]:w-[150px] [&_span]:mb-[-15px] px-[30px]"
+            >
+              <div className="flex gap-[30px] h-[50px] w-full  mb-6">
+                <MyInput
+                  text="Имя"
+                  control={control}
+                  name="first_name"
+                  defaultValue={finalData?.user?.first_name}
+                  required
+                  setValue={setValue}
+                />
+                <MyInput
+                  text="Фамилия"
+                  defaultValue={finalData?.user?.last_name}
+                  control={control}
+                  name="last_name"
+                  required
+                  setValue={setValue}
+                />
+                <MyInput
+                  text="Отчество"
+                  defaultValue={finalData?.user?.patronymic}
+                  control={control}
+                  name="patronymic"
+                  setValue={setValue}
+                />
+              </div>
+              <div className="flex flex-col w-1/2 gap-6">
+                <MyInput
+                  text="Телефон"
+                  defaultValue={finalData?.user?.phone}
+                  control={control}
+                  name="phone"
+                  setValue={setValue}
+                />
+
+                <MyInput
+                  text="E-mail"
+                  defaultValue={finalData?.user?.email}
+                  control={control}
+                  name="email"
+                  setValue={setValue}
+                />
+              </div>
+              <div className="bg-[#F9F9F9] flex justify-between items-center pr-8 h-[107px] w-full border-y-1 border-[#d9d9d9]">
+                <h3 className="text-30 py-8 pl-8 ">{"Способ оплаты"}</h3>
+              </div>
+              <div className="pl-8">
+                <div className="flex justify-start gap-3">
+                  <div onClick={() => setPayCash(true)} className={choosenCash}>
+                    <p className="align-middle">{payCash ? "✓" : ""}</p>
+                  </div>
+                  <p className={payCash ? "text-orange" : "text-slate-700"}>Наличными</p>
+                </div>
+              </div>
+              <div className="border-t-1 border-slate-400 border-dotted py-8 pl-8">
+                <div onClick={() => setPayCash(false)} className="flex justify-start gap-3">
+                  <div className={choosenOnline}>
+                    <p>{payCash ? " " : "✓"}</p>
+                  </div>
+                  <p className={!payCash ? "text-orange" : "text-slate-700"}>Онлайн</p>
+                </div>
+                <div className="flex gap-12 py-4 mt-4">
+                  <p className="text-slate-700">Банковской картой</p>
+                  <p className="text-slate-700">PayPal</p>
+                  <p className="text-slate-700">Visa QIWI Wallet</p>
+                </div>
+              </div>
+              <div className="flex justify-between mt-6">
+                <MyButton
+                  className={" w-[250px] p-4 h-[100px]"}
+                  label="Назад"
+                  color="transparent"
+                  onClick={() => goBack()}
+                ></MyButton>
+                <MyButton
+                  className={" w-[250px] p-4 h-[100px]"}
+                  label="купить билеты"
+                  color="orange"
+                  onClick={() => {
+                    onSubmit();
+                    // setNextPage(true);
+                  }}
+                ></MyButton>
+              </div>
+            </form>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
